3febc587e61eae5516027fbf42a6fc4b2052a220
XeniaOlshanskaya <ks14012000@gmail.com>
2019-04-29 00:54:23 +0700

    2

--- case.py
+++ case.py
@@ -1,13 +1,43 @@
 from hotel import *
 
+variants_dict = {}
 price_list = {1: 2900, 2: 2300, 3: 3200, 4: 4100}
 comfort_list = {1: 1.0, 2: 1.2, 3: 1.5}
+food_dict = {1: 0.0, 2: 280.0, 3: 1000.0}
+big_list = []
+
 with open("fund.txt", "r", encoding="UTF-8-sig") as rooms:
     text = rooms.readlines()
-    # Обрабатываем и отправляем в класс Room, чтобы сгенерировать объекты (какие номера есть)
     for i in text:
         room1 = i.split()
-        number = room1[0], type_ = room1[1], human_quantity = room1[2], comfort_degree = room1[3]
-        current_room = Room(number, type_, human_quantity, comfort_degree)
+        number = room1[0]
+        type_ = room1[1]
+        human_quantity = room1[2]
+        comfort_degree = room1[3]
+        current_room = Choice(number, type_, human_quantity, comfort_degree)
         current_room.count_price(price_list, comfort_list)
+
+        for t in range(1, int(current_room.human_quantity)+1):
+            for n in range(1, 4):
+                room_list = []
+                current_room.choose(t, food_dict[n])
+                cur_room = Choice(current_room.number,
+                                  current_room.type_,
+                                  current_room.human_quantity,
+                                  current_room.comfort_degree,
+                                  current_room.price,
+                                  current_room.human_fact,
+                                  current_room.nutrition_type)
+                room_list.append(current_room.price)
+                room_list.append(cur_room)
+                big_list.append(room_list)
+                current_room.price -= food_dict[n]
+            current_room.price /= t
+    big_list = sorted(big_list, key=lambda tup: tup[0])
+    for i in big_list:
+        print(i)
+
+
+
+
 
--- hotel.py
+++ hotel.py
@@ -1,9 +1,10 @@
 class Room:
-    def __init__(self, number, type_, human_quantity, comfort_degree):
+    def __init__(self, number, type_, human_quantity, comfort_degree, price=0):
         self.number = number
         self.type_ = type_
         self.human_quantity = human_quantity
         self.comfort_degree = comfort_degree
+        self.price = price
 
     def __str__(self):
         s = str(self.number)
@@ -18,13 +19,13 @@ class Room:
 
     def count_price(self, price_list, comfort_list):
         if self.type_ == "одноместный":
-            self.price = int(self.human_quantity) * price_list[1]
+            self.price = price_list[1]
         elif self.type_ == "двухместный":
-            self.price = int(self.human_quantity) * price_list[2]
+            self.price = price_list[2]
         elif self.type_ == "полулюкс":
-            self.price = int(self.human_quantity) * price_list[3]
+            self.price = price_list[3]
         else:
-            self.price = int(self.human_quantity) * price_list[4]
+            self.price = price_list[4]
         if self.comfort_degree == "стандарт":
             pass
         elif self.comfort_degree == "стандарт_улучшеный":
@@ -32,3 +33,39 @@ class Room:
         else:
             self.price *= comfort_list[3]
 
+
+class Choice(Room):
+    def __init__(self, number, type_, human_quantity, comfort_degree, price=0, human_fact=0, nutrition_type=0,
+                 status=False):
+        super().__init__(number, type_, human_quantity, comfort_degree, price)
+        self.human_fact = human_fact
+        self.nutrition_type = nutrition_type
+        self.status = status    # Если статус False, значит номер не занят, ниже есть функция,
+                                # которая меняет статус
+
+    def __str__(self):
+        s = "номер " + str(self.number) + " " + self.type_ + " " + self.comfort_degree
+        s += " рассчитан на " + str(self.human_quantity) + " человек "
+        s += "фактически " + str(self.human_fact) + " чел. " + str(self.nutrition_type)
+        s += " стоимость " + str(self.price) + " руб./сутки"
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+
+    def status_change(self):
+        self.status = True
+        return self.status
+
+    def choose(self, t, food_type):
+        self.human_fact = t
+        self.nutrition_type = food_type
+        self.price += self.nutrition_type
+        self.price *= self.human_fact
+        if int(self.human_quantity) > t:
+            self.price *= 0.7
+        self.price = round(self.price, 2)
+        return self.price
+
+class Hotel:
+    pass    # Класс занятости номеров
